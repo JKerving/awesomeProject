d812d2d3cdb58f315f2352f1df621f84dd05f39e
JKerving <zjw18603166118@sina.cn>
2018-06-06 16:51:45 +0800

    struct and pointer Test

--- StructTest/structTest.go
+++ StructTest/structTest.go
@@ -7,6 +7,12 @@ type Person struct {
 	age int
 }
 
+type Student struct {
+	name string
+	id int
+	score int
+}
+
 func main() {
 	fmt.Println(Person{"Bob",20})
 	fmt.Println(Person{name:"Alice",age:30})
@@ -31,4 +37,26 @@ func main() {
 	x:=[3]*int{&m[0],&m[1],&m[2]}
 	fmt.Printf("x:%v,%v,%v\n",x[0],x[1],x[2])
 	fmt.Printf("*x:%v,%v,%v\n",*x[0],*x[1],*x[2])
+
+	var n [3]*int
+	n = x
+	fmt.Printf("n:%v,%v,%v\n",n[0],n[1],n[2])
+	y:=[]*[3]*int{&x}
+	fmt.Printf("y:%v,%v\n",y,y[0])
+	fmt.Printf("*y[0]:%v\n",*y[0])
+	fmt.Printf("*y[][]:%v,%v,%v\n",*y[0][0],*y[0][1],*y[0][2])
+
+	var st Student
+	st.name="Chinese"
+	st.id=666333
+	st.score=100
+	fmt.Println(st)
+	var ptr *[]Student
+	fmt.Println(ptr)
+	ptr=new([]Student)
+	fmt.Println(ptr)
+	*ptr=make([]Student,3,100)
+	fmt.Println(ptr)
+	stu:=[]Student{{"China",3333,66},{"Chinese",4444,77},{"Chince",5555,88}}
+	fmt.Println(stu)
 }
