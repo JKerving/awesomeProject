83d16dd2ad5bc4699463f190b77e04652536db82
JKerving <zjw18603166118@sina.cn>
2018-07-25 14:45:16 +0800

    panic Test

--- PanicTest/panicTest.go
+++ PanicTest/panicTest.go
@@ -48,12 +48,38 @@ func afterErrorfunc() {
 	fmt.Println("遇到错误之后 func")
 }
 
-func main() {
-	err:=testError()
-	if err!=nil{
-		fmt.Println("main函数得到错误类型:",err)
+func fullName(firstName *string, lastName *string) {
+	defer recoverName()
+	if firstName==nil{
+		panic("runtime error:;first name cannot be nil")
 	}
-	afterErrorfunc()
+	if lastName==nil{
+		panic("runtime error:last name cannot be nil")
+	}
+	fmt.Printf("%s %s\n",*firstName,*lastName)
+	fmt.Println("returned normally from fullName")
+}
+
+func recoverName()  {
+	if r := recover(); r != nil {
+		fmt.Println("recovered from",r)
+	}
+}
+
+func main() {
+	//err:=testError()
+	//if err!=nil{
+	//	fmt.Println("main函数得到错误类型:",err)
+	//}
+	//afterErrorfunc()
+	//defer fmt.Println("deferred call in main")
+	//firstName:="Elon"
+	//fullName(&firstName,nil)
+	//fmt.Println("returned normally from main")
+	i:=0
+	defer fmt.Println(i)
+	i++
+	return
 }
 
 func divide(i, j int) {
